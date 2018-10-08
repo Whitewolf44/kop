80924e64febedd6e6bbdd751d48da007c5ee2cb0
Whitewolf44 <sgkfbk@gmail.com>
2018-10-08 16:25:03 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="android-icon-192x192.png">
+    <link rel="icon" t
+    ype="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Novo Szrabia</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+
+
+<div class="header"> 
+
+    <div class="headertop">  </div>
+    <div class="headerbot"> 
+    <li class="menu"> <a href= "index.php"> Főoldal </a> </li> 
+    <li class="menu"> <a href= "kultura.php"> Kultúra  </a> </li> 
+    <li class="menu"><a href= "tortenelem.php"> Történelem </a></li> 
+    <li class="menu"> <a href= "galeria.php">Galéria</a></li> 
+    <li class="menu"> <a href= "infok.php">Információk</a></li> </ul>
+    </div>
+</div>
+
+
+
+<div class="content"> 
+
+
+
+        <div class="section" id="nyilt"> <h2> Nyílt kereskedelem </h2>
+        <img src="hir3.png" class="section-img">  
+
+
+        <p> 
+        <b>03.09.2000</b> <br>
+       Egy hosszú folyamat lezárásaképp a rendőrség ismét átvette az irányítást a háború kezdete óta elhagyatott határátkelőkön.
+        Így a határ kontroll visszakerült hivatalos kezekbe. Egyelőre ugyan nem bonyolítanak túlságosan nagy forgalmat, viszont a kereskedelem máris megindult
+         Csehoszlávia felől így ismételten értékes külföldi áruk folynak be a boltok polcaira.
+         
+     </p>
+        </div>
+
+        <div class="section2"> <h2 class="hketto"> Újjáépítési rendelet </h2>
+        <br><br><br>
+        <img src="hir1.png" class="section-img2">  
+
+        <p> 
+        <b>03.08.2000</b> <br>
+        A Szráb népi parlament ma egy új rendeletet szavazott meg miszerint a polgárháború utáni újjépítéshez prioritáslistára kerülnek a kórházak és az utak. 
+        Azonban elsősorban a még menthető állapotúakkal fognak foglalkozni. Közben pedig igyekeznek visszaállítani a vízellátást a kisebb településeken is mivel a
+         központi városokban már nem tapasztalni problémákat.
+        <a href=*> Olvass tovább... </a>
+     </p>
+        </div>
+
+    <div class="section"> 
+    <h2> Polgárháború vége </h2> 
+    
+        <img src="hir2.jpg" class="section-img">  
+
+        <p> 
+        <b>08.05.1999</b> <br>
+        A mai napon hat év véres konfliktus után véget értek a harcok hazánkban. Katonáink vér és veríték árán kiszorították a Pánszláv únió utolsó emberét is a
+         Dolma vidékéről. A Viharfelhő hadművelet során csapataink szinte minimális ellenállásba ütköztek ahogy felszabadították az utolsó Szráb falut is az 
+         északi hegységekben. 
+         <a href=*> Olvass tovább... </a>
+
+     </p>
+        </div>
+
+
+</div>
+<div class="footer"> <img src="favicon-96x96.png" class="logo"> 
+        <ul class="botul"> <li class="botli">Telefon: +421948667781 </li>  
+        <li class="botli">Facebook: <a href= "https://www.facebook.com/Novo-Szrabia-307207006679817/?modal=admin_todo_tour"> Novo Szrabia Facebook </a> 
+        </li>  <li class="botli">Cím: Szkoj, Sztrogojov utca 22.</li> </ul>
+
+<div class="footer2">
+    Made by: Whitewolf
+    
+    </div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,15 @@
+setInterval(3000);
+<img src="..." id="myImage">
+
+
+<script type="text/javascript">
+jQuery(function(){
+
+   // Fade In
+   $("#myImage").fadeIn();
+
+   // Fade Out
+   $("#myImage").fadeOut();
+
+});
+</script>
